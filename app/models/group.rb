4f5a91ff0678dc312a143765982b72bda81aa300
GerardBiehl <gerardbiehl@gmail.com>
2014-05-06 20:18:27 -0500

    Created url regex and length rules

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_URL_REGEX = /\A[\w+(\-.|\_.)]/i
+  validates :url, presence: true, format: {with: VALID_URL_REGEX }, length: { maximum: 20 }
 end

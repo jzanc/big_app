8235196c4dc9221d7d96399ddaf639a54aa47929
GerardBiehl <gerardbiehl@gmail.com>
2014-05-06 22:15:51 -0500

    Finished create group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,24 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to @group
+    else
+      render 'new'
+    end
   end
 
   def show
     @group = Group.find(params[:id])
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :url)
   end
 end
--- spec/requests/group_pages_spec.rb
+++ spec/requests/group_pages_spec.rb
@@ -11,6 +11,28 @@ describe "Group Pages" do
     it { should have_title(full_title('Create Group')) }
   end  
 
+  describe "create group" do
+    before { visit create_group_path }
+    let(:submit) { "Create Group" }
+
+    describe "with invalid information" do
+      it "should not create a group" do
+        expect { click_button submit }.not_to change(Group, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example Group"
+        fill_in "Url" , with: "Example_Url"
+      end
+
+      it "should create a group" do
+        expect { click_button submit }.to change(Group, :count).by(1)
+      end
+    end
+  end
+
   describe "group page" do
     let(:group) { FactoryGirl.create(:group) }
     before { visit group_path(group) }
